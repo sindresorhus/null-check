448f46440ff312e3f17f69f813ca407dc88840aa
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-17 23:34:31 +0200

    Use throws and doesNotThrow in

--- test.js
+++ test.js
@@ -2,19 +2,11 @@ import test from 'ava';
 import fn from './';
 
 test('null bytes', t => {
-	try {
-		fn('unicorn.png\u0000');
-		t.fail('Error not thrown.');
-	} catch (err) {
-		t.ok(err);
-		t.is(err.message, 'Path must be a string without null bytes.');
-		t.is(err.code, 'ENOENT');
-	} finally {
-		t.end();
-	}
+	t.throws(fn.bind(null, 'unicorn.png\u0000'), Error, 'Path must be a string without null bytes.');
+	t.end();
 });
 
 test('no null bytes', t => {
-	fn('unicorn.png');
+	t.doesNotThrow(fn.bind(null, 'unicorn.png'));
 	t.end();
 });
