1d181efe9cb7eb1c13b8ecc1f9e9cad76fb19eb0
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 23:35:03 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -4,5 +4,4 @@ import fn from './';
 test(t => {
 	t.throws(() => fn('unicorn.png\u0000'), Error, 'Path must be a string without null bytes.');
 	t.doesNotThrow(() => fn('unicorn.png'));
-	t.end();
 });
